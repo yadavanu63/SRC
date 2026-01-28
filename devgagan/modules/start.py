ce991952a2152284fcd5f8effbc05c4caa58cd0b
ANU YADAV <163430575+yadavanu63@users.noreply.github.com>
2026-01-28 14:04:29 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -183,7 +183,7 @@ async def terms(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/AJS2003")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/taporibot_bot")],
         ]
     )
     await message.reply_text(terms_text, reply_markup=buttons)
@@ -202,7 +202,7 @@ async def plan(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/chiru52")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/taporibot_bot")],
         ]
     )
     await message.reply_text(plan_text, reply_markup=buttons)
@@ -221,7 +221,7 @@ async def see_plan(client, callback_query):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/AJS2003")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/taporibot_bot")],
         ]
     )
     await callback_query.message.edit_text(plan_text, reply_markup=buttons)
@@ -239,7 +239,7 @@ async def see_terms(client, callback_query):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/AJS2003")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/taporibot_bot")],
         ]
     )
     await callback_query.message.edit_text(terms_text, reply_markup=buttons)
