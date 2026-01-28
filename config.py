223e95db5c86e1fad8acdb8014aac66d2491e429
ANU YADAV <163430575+yadavanu63@users.noreply.github.com>
2026-01-28 13:36:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,16 +12,16 @@ INST_COOKIES = """
 YTUB_COOKIES = """
 # write here yt cookies
 """
-img_url = getenv("img_url","https://imgs.search.brave.com/THqPKczwvPZMcP7ME81cY86Y_zf3C5TvrUIkce6YNCo/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9yMi5z/dGFycnlhaS5jb20v/cmVzdWx0cy8xMDA3/MzQ3OTcwLzk2NzVj/YjQzLWE4YmEtNGYy/Ny05NmU5LTYzYTNk/NzVlYTQ5Zi53ZWJw")
-Credit = getenv("Credit","Star AJ")
-c_url = getenv("c_url","https://t.me/+N5xSRah10TZlNmY1")
+img_url = getenv("img_url","https://i.ibb.co/V0NnQFGP/file-881.jpg")
+Credit = getenv("Credit","𝗧𝗮𝗽𝗼𝗿𝗶 𝟮.𝟬🥷")
+c_url = getenv("c_url","https://t.me/tapori420_Official")
 API_ID = int(getenv("API_ID", "27765349"))
 API_HASH = getenv("API_HASH", "9df1f705c8047ac0d723b29069a1332b")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1116405290").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6127347210").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "-1002409813500")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002409813500"))
+LOG_GROUP = getenv("LOG_GROUP", "-1002930828750")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002226781947"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "10000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "indianshortner.com")
